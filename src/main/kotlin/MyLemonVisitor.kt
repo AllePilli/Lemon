804999029269d474fb174052f1262272534f9c17
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2019-04-25 11:54:39 +0200

    Added visitArithmeticExpressionMult method

--- src/main/kotlin/MyLemonVisitor.kt
+++ src/main/kotlin/MyLemonVisitor.kt
@@ -7,27 +7,20 @@ class MyLemonVisitor: LemonBaseVisitor<Value>() {
     // Variable storage (only 1 global scope)
     private val memory: MutableMap<String, Value> = mutableMapOf()
 
-    /**
-     * {@inheritDoc}
-     *
-     *
-     * The default implementation returns the result of calling
-     * [.visitChildren] on `ctx`.
-     */
     override fun visitDeclaration(ctx: LemonParser.DeclarationContext?): Value {
         val identifier = ctx!!.IDENTIFIER().text
         val value = visit(ctx.expression())
         return memory.put(identifier, value) ?: Value.VOID
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     *
-     * The default implementation returns the result of calling
-     * [.visitChildren] on `ctx`.
-     */
     override fun visitExpressionArithmetic(ctx: LemonParser.ExpressionArithmeticContext?) = visit(ctx!!.arithmetic_expression())
+
+    override fun visitArithmeticExpressionMult(ctx: LemonParser.ArithmeticExpressionMultContext?): Value {
+        val left = visit(ctx!!.arithmetic_expression(0))
+        val right = visit(ctx.arithmetic_expression(1))
+
+        return Value(left.asDouble() * right.asDouble())
+    }
 
     /**
      * Prints the memory to the console

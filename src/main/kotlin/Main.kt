76357b95a92d9c1ffee5055b03e28b90129f9096
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2019-04-25 11:33:05 +0200

    Added for testing

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import org.antlr.v4.runtime.ANTLRFileStream
+import org.antlr.v4.runtime.CommonTokenStream
+
+const val root = "C:\\Users\\alles\\OneDrive\\Documents\\KotlinProjects\\Lemon\\src\\main\\antlr\\test.lemon"
+
+fun main() {
+    val input = ANTLRFileStream(root)
+
+    val lexer = LemonLexer(input)
+    val parser = LemonParser(CommonTokenStream(lexer))
+
+    val visitor = MyLemonVisitor()
+    val result = visitor.visit(parser.declaration())
+}
